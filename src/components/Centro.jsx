cca9195ba4021f3eb2b15f9c5eec51163157080c
wiliam <morillow052@gmail.com>
2025-06-04 12:29:59 -0400

    vericando la pag de productos

--- src/components/Centro.jsx
+++ src/components/Centro.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 import { Buton } from "./Buton";
 import { useContext } from "react";
 import { DatosCarritoContext } from "../componentesfail/DatosParaCarrito";
@@ -10,7 +10,7 @@ export function Centro() {
     []
   );
   const { setContadorArticulos } = useContext(DatosCarritoContext);
-
+const navigate = useNavigate();
   const urlProducots =
     "https://tienda-production-852a.up.railway.app/productos";
 
