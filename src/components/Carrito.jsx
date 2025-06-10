bcc07943ce824627f3df9041bf0ad97508aa1b03
William Morillo <morillow02@gmail.com>
2025-06-10 00:50:16 -0400

    estilando

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -64,7 +64,6 @@ export function Carrito() {
   }
   return (
     <div className="relative">
-      {/* Icono del carrito y contador */}
       <div className="flex items-center gap-2 cursor-pointer">
         <ShoppingCart size={32} color="blue" />
         <span
@@ -75,7 +74,6 @@ export function Carrito() {
         </span>
       </div>
 
-      {/* Modal del carrito */}
       {mostrarModal && (
         <div className="absolute top-10 right-0 w-80 bg-white border border-gray-300 shadow-lg rounded-lg p-4 z-50">
           <ul className="space-y-4 max-h-80 overflow-y-auto">
@@ -105,7 +103,7 @@ export function Carrito() {
           </ul>
 
           {/* Botones */}
-          <div className="flex flex-col gap-2 mt-4">
+          <div className="flex flex-col gap-2 mt-4 justify-center items-center">
             <button
               onClick={HandleCarrito}
               className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded transition-colors"
