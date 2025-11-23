67be0cdc080eaf1779b012825c5b191b1e69dd78
William Morillo <morillow052@gmail.com>
2025-11-23 02:44:21 -0400

    resolviendo el buton de crearProductos que se pierde al recargar

--- src/componentesfail/PermisosId.jsx
+++ src/componentesfail/PermisosId.jsx
@@ -1,13 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const PermisosContext = createContext();
 
 export const PermisosId = ({ children }) => {
-  const [usuarioId, setUsuarioId] = useState(null);
+  const [usuarioId, setUsuarioId] = useState(() => {
+    //  esto sirve para leer del localStorage al iniciar porque el use context solo sirve en memoria
+    return localStorage.getItem("usuarioId") || null;
+  });
+
+  useEffect(() => {
+    if (usuarioId) {
+      localStorage.setItem("usuarioId", usuarioId);
+    } else {
+      localStorage.removeItem("usuarioId");
+    }
+  }, [usuarioId]);
 
   return (
     <PermisosContext.Provider value={{ usuarioId, setUsuarioId }}>
       {children}
     </PermisosContext.Provider>
   );
-};
+}
